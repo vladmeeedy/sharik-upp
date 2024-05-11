b74bf41c8118d1280de8930448494305f214e53c
Volodymyr Medvedchuk <vladmeeedy@gmail.com>
2024-05-11 19:22:12 +0300

    add figure to productsData.json file

--- src/redux/balloons/asyncActions.js
+++ src/redux/balloons/asyncActions.js
@@ -12,7 +12,7 @@ export const fetchBallons = createAsyncThunk(
     } else if (currentPage == 2) {
       indices = [8, 9, 10, 11, 12, 13, 14, 15];
     } else if (currentPage == 3) {
-      indices = [16, 17, 18, 19, 20, 21, 22];
+      indices = [16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43];
     }
 const result = indices.map(index => data[index]);
    
