3fc32ea5de5eab7a731ac059a8bdcf74eb67b457
Volodymyr Medvedchuk <vladmeeedy@gmail.com>
2024-07-15 17:29:03 +0300

    add changes to translation

--- client/src/components/CartEmpty.js
+++ client/src/components/CartEmpty.js
@@ -1,39 +1,39 @@
-import cartEmptyImg from '../assets/images/empty-cart.png'
-import { Link, NavLink } from 'react-router-dom'
+import cartEmptyImg from '../assets/images/empty-cart.png';
+import { Link, NavLink } from 'react-router-dom';
+import { useTranslation } from 'react-i18next';
 
 export const CartEmpty = () => {
+  const { t } = useTranslation();
+
   return (
     <div className="cart cart--empty">
       <h2>
-        Товары отсутствуют <span>😕</span>
+        {t('cart.empty.title')} <span>😕</span>
       </h2>
       <p>
-        Возможно, вы еще не заказали товары.
+        {t('cart.empty.message')}
         <br />
-        Для заказа, перейдите на главную страницу.
+        {t('cart.empty.instruction')}
       </p>
-      <img src={cartEmptyImg} alt="Empty cart" />
-      <NavLink
-              to="/"
-              className="go-back-btn"
-            >
-              <svg
-                width="8"
-                height="14"
-                viewBox="0 0 8 14"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  d="M7 13L1 6.93015L6.86175 1"
-                  stroke="#D3D3D3"
-                  strokeWidth="1.5"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                ></path>
-              </svg>
-              <span>Продолжить покупки</span>
-            </NavLink>
+      <img src={cartEmptyImg} alt={t('cart.empty.imgAlt')} />
+      <NavLink to="/" className="go-back-btn">
+        <svg
+          width="8"
+          height="14"
+          viewBox="0 0 8 14"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            d="M7 13L1 6.93015L6.86175 1"
+            stroke="#D3D3D3"
+            strokeWidth="1.5"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          ></path>
+        </svg>
+        <span>{t('cart.empty.buttonText')}</span>
+      </NavLink>
     </div>
-  )
-}
+  );
+};
