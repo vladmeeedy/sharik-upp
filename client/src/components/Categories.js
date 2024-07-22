63632332a778e2aebaa96e705e9b76a238a4c7fc
Volodymyr Medvedchuk <vladmeeedy@gmail.com>
2024-07-22 14:37:08 +0300

    build

--- client/src/components/Categories.js
+++ client/src/components/Categories.js
@@ -1,33 +1,39 @@
-import React, { useMemo } from 'react';
-import { NavLink } from 'react-router-dom';
-import { useTranslation } from 'react-i18next';
+import React, { useMemo } from 'react'
+import { NavLink } from 'react-router-dom'
+import { useTranslation } from 'react-i18next'
 
 export const Categories = React.memo(() => {
-  const { t } = useTranslation();
+  const { t } = useTranslation()
 
-  const links = useMemo(() => [
-    { to: "/", label: t('categoryHomeBalloons') },
-    { to: "/gelievye-shary", label: t('categoryLatexBalloons') },
-    { to: "/folgirovannye-figury", label: t('categoryFiguresBalloons') },
-    { to: "/folgirovannye-zvezdy", label: t('categoryStarsBalloons') },
-    { to: "/folgirovannye-cifry", label: t('categoryNumbersBalloons') },
-    { to: "/korobka-syurpriz", label: t('categorySurpriseBox') },
-    { to: "/svechi-na-tort", label: t('categoryCandlesCake') }
-  ], [t]);
+  const links = useMemo(
+    () => [
+      { to: '/', label: t('categoryHomeBalloons') },
+      { to: '/gelievye-shary', label: t('categoryLatexBalloons') },
+      { to: '/folgirovannye-figury', label: t('categoryFiguresBalloons') },
+      { to: '/folgirovannye-zvezdy', label: t('categoryStarsBalloons') },
+      { to: '/folgirovannye-cifry', label: t('categoryNumbersBalloons') },
+      { to: '/korobka-syurpriz', label: t('categorySurpriseBox') },
+      { to: '/svechi-na-tort', label: t('categoryCandlesCake') },
+    ],
+    [t],
+  )
 
   return (
     <div className="categories">
       <ul>
-        {links.map(link => (
+        {links.map((link) => (
           <NavLink
             key={link.to}
             to={link.to}
             className={({ isActive }) => (isActive ? 'active' : '')}
+            onClick={(e) => {
+              e.stopPropagation() // Предотвращаем дальнейшее распространение события
+            }}
           >
             <p>{link.label}</p>
           </NavLink>
         ))}
       </ul>
     </div>
-  );
-});
+  )
+})
