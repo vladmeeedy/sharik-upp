e52633ba005beea7ff51dbd8c354aeb948169739
Volodymyr Medvedchuk <vladmeeedy@gmail.com>
2024-07-24 17:46:29 +0300

    server puth changed

--- server/api/index.mjs
+++ server/api/index.mjs
@@ -12,7 +12,7 @@ const TELEGRAM_CHAT_ID = "675843760";
 app.use(cors());
 app.use(bodyParser.json());
 
-app.post("/api/sendOrder", (req, res) => {
+app.post("/", (req, res) => {
   const { buyerInfo, cartItems } = req.body;
 
   console.log("Received order:", req.body);
