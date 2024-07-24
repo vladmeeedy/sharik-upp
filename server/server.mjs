c9d4cbf51d4bfa9f01c048047e22347a32e6f8a2
Volodymyr Medvedchuk <vladmeeedy@gmail.com>
2024-07-24 15:45:01 +0300

    changed server

--- server/server.mjs
+++ server/server.mjs
@@ -1,22 +1,24 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-import cors from 'cors';
-import fetch from 'node-fetch';
+import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
+import fetch from "node-fetch";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-const TELEGRAM_API_TOKEN = '7438927464:AAEtXYOVk80bXFme_dKfApdqD93EOdpQkNo';
-const TELEGRAM_CHAT_ID = '675843760'; 
+const TELEGRAM_API_TOKEN = process.env.TELEGRAM_API_TOKEN;
+const TELEGRAM_CHAT_ID = process.env.TELEGRAM_CHAT_ID;
 
 app.use(cors());
 app.use(bodyParser.json());
 
-app.post('/api/sendOrder', (req, res) => {
+app.post("/api/sendOrder", (req, res) => {
   const { buyerInfo, cartItems } = req.body;
 
-  // Отладочные выводы
-  console.log('Received order:', req.body);
+  console.log("Received order:", req.body);
 
   const message = `
     Новый заказ:
@@ -28,28 +30,31 @@ app.post('/api/sendOrder', (req, res) => {
     Дата доставки: ${buyerInfo.deliveryDate}
     
     Товары:
-    ${cartItems.map(item => `- ${item.title}, количество: ${item.count}`).join('\n')}
+    ${cartItems
+      .map((item) => `- ${item.title}, количество: ${item.count}`)
+      .join("\n")}
   `;
 
-  // Отладочные выводы для сообщения
-  console.log('Telegram message:', message);
+  console.log("Telegram message:", message);
 
   const telegramMessage = encodeURIComponent(message);
 
-  fetch(`https://api.telegram.org/bot${TELEGRAM_API_TOKEN}/sendMessage?chat_id=${TELEGRAM_CHAT_ID}&text=${telegramMessage}`)
-    .then(response => response.json())
-    .then(data => {
+  fetch(
+    `https://api.telegram.org/bot${TELEGRAM_API_TOKEN}/sendMessage?chat_id=${TELEGRAM_CHAT_ID}&text=${telegramMessage}`
+  )
+    .then((response) => response.json())
+    .then((data) => {
       if (data.ok) {
-        console.log('Сообщение в Telegram отправлено:', data);
-        res.status(200).send('Уведомление успешно отправлено');
+        console.log("Сообщение в Telegram отправлено:", data);
+        res.status(200).send("Уведомление успешно отправлено");
       } else {
-        console.error('Ошибка отправки сообщения в Telegram:', data);
-        res.status(500).send('Ошибка отправки уведомления в Telegram');
+        console.error("Ошибка отправки сообщения в Telegram:", data);
+        res.status(500).send("Ошибка отправки уведомления в Telegram");
       }
     })
-    .catch(error => {
-      console.error('Ошибка при отправке запроса в Telegram API:', error);
-      res.status(500).send('Ошибка отправки уведомления в Telegram');
+    .catch((error) => {
+      console.error("Ошибка при отправке запроса в Telegram API:", error);
+      res.status(500).send("Ошибка отправки уведомления в Telegram");
     });
 });
 
