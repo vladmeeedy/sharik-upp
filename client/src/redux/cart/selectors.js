b1a0071af5e6f8e90f15e57ed786373227c73c5e
Volodymyr Medvedchuk <vladmeeedy@gmail.com>
2024-07-30 14:55:34 +0300

    changed selector

--- client/src/redux/cart/selectors.js
+++ client/src/redux/cart/selectors.js
@@ -1,4 +1,3 @@
-import { createSelector } from 'reselect'
-export const selectCart = state => state.cart
-export const selectCartItemById = createSelector((id) => (state) =>
-  state.cart.items.find((obj) => obj.id === id))
+export const selectCart = (state) => state.cart
+export const selectCartItemById = (id) => (state) =>
+  state.cart.items.find((obj) => obj.id === id)
